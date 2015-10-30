4ab4e68644538abee2473423716b14ae4b6ee901
Maciej Kucharski <maciek.zir@gmail.com>
2015-10-30 18:16:48 +0100

    [master] @alpha.45 routing fix

--- shop/src/app/bootstrap.ts
+++ shop/src/app/bootstrap.ts
@@ -1,9 +1,13 @@
 import {bootstrap, bind} from 'angular2/angular2';
 import {
-    ROUTER_BINDINGS,
+    ROUTER_PROVIDERS,
     APP_BASE_HREF,
-    ROUTER_PRIMARY_COMPONENT
+    ROUTER_PRIMARY_COMPONENT,
+    HashLocationStrategy,
+    LocationStrategy,
+    PathLocationStrategy
 } from 'angular2/router';
+
 import {
     HTTP_BINDINGS
 } from 'angular2/http';
@@ -14,10 +18,12 @@ import {DataService} from './services/data';
 /* root component */
 import {RootComponent} from './components/app';
 
+
 const ROUTER_CONFIG = [
-    ROUTER_BINDINGS,
-    bind(APP_BASE_HREF).toValue('/src'),
-    bind(ROUTER_PRIMARY_COMPONENT).toValue(RootComponent)
+    ROUTER_PROVIDERS,
+    bind(APP_BASE_HREF).toValue('/'),
+    bind(ROUTER_PRIMARY_COMPONENT).toValue(RootComponent),
+    bind(LocationStrategy).toClass(HashLocationStrategy)
 ];
 const HTTP_CONFIG = [
     HTTP_BINDINGS
